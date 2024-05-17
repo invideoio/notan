91fd379adab7468bb0e6b88ee20aa60dc5ae0dd7
fand <mail@amagi.dev>
2024-05-17 02:13:20 -0700

    Revert "perf: set canvas size only if necessary"
    This reverts commit 375d8d5491a6a05b6f4ff8086ac0f918d15e2f6f.

--- crates/notan_web/src/window.rs
+++ crates/notan_web/src/window.rs
@@ -370,11 +370,9 @@ impl WindowBackend for WebWindowBackend {
     fn set_position(&mut self, _x: i32, _y: i32) {}
 
     fn set_size(&mut self, width: i32, height: i32) {
-        if width != self.config.width || height != self.config.height {
-            set_size_dpi(&self.canvas, width as _, height as _);
-            self.config.width = width;
-            self.config.height = height;
-        }
+        set_size_dpi(&self.canvas, width as _, height as _);
+        self.config.width = width;
+        self.config.height = height;
     }
 
     fn set_visible(&mut self, visible: bool) {
