ee0a60f1d6c4c7edd4d5d18c94e43d5dbba587b1
AMAGI / Jun Yuri <mail@amagi.dev>
2026-03-04 22:11:21 -0800

    fix: pin rust-overlay version for Rust 1.67.0

--- nix/rust-overlay.nix
+++ nix/rust-overlay.nix
@@ -1,1 +1,1 @@
-(import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+(import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/c1df023b1aaded1b65a1f4ad604a98a58ab4db97.tar.gz"))
